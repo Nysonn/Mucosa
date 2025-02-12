1f90caaa1d2df9d3e7eef3b4f8d63c6908fc59f5
LiamBolt <amwineliam@gmail.com>
2025-02-12 20:41:45 +0300

    .

--- server/config/settings.py
+++ server/config/settings.py
@@ -169,9 +169,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Django REST Framework configuration
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
