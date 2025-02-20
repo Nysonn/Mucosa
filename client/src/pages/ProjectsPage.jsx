716030854322d368ac18d1f4dee1da13bc4bb774
Nysonn <nysonagumya@gmail.com>
2025-02-20 13:23:33 +0300

    Separate Project Card Component

--- client/src/pages/ProjectsPage.jsx
+++ client/src/pages/ProjectsPage.jsx
@@ -1,47 +1,7 @@
 import React from 'react';
 import styles from './ProjectsPage.module.css';
 import useProjects from '../hooks/useProjects';
-import { highlightText } from '../utils/highlightText';
-
-function ProjectCard({ title, description, image, link, tech, searchQuery }) {
-  return (
-    <div className={styles.projectCard}>
-      <div className={styles.imageContainer}>
-        <div
-          className={styles.projectImage}
-          style={{ backgroundImage: `url(${image})` }}
-        />
-        <div className={styles.overlay}>
-          <div className={styles.links}>
-            <a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.link}
-            >
-              Github
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className={styles.content}>
-        <h3 className={styles.projectTitle}>
-          {highlightText(title, searchQuery)}
-        </h3>
-        <p className={styles.projectDescription}>
-          {highlightText(description, searchQuery)}
-        </p>
-        <div className={styles.techStack}>
-          {tech.map((item, index) => (
-            <span key={index} className={styles.techItem}>
-              {item}
-            </span>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-}
+import ProjectCard from '../components/ProjectCard/ProjectCard';
 
 export default function ProjectsPage() {
   // Destructure the required state and functions from the custom hook
