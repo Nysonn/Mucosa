a8eafa28672be030a8689f2ff55bbe29b7590411
Nysonn <nysonagumya@gmail.com>
2025-02-20 14:01:52 +0300

    Applying Lazy Loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
-import HomePage from './pages/HomePage';
-import NewsPage from './pages/NewsPage';
-import EventsPage from './pages/EventsPage';
-import CareerPage from './pages/CareerPage';
-import AboutPage from './pages/AboutPage';
-import ProjectPage from './pages/ProjectsPage';
-import NewsDetailPage from './pages/NewsDetailPage';
-import ErrorPage from '../src/components/common/Error';
+
+// Lazy load route components
+const HomePage = lazy(() => import('./pages/HomePage'));
+const NewsPage = lazy(() => import('./pages/NewsPage'));
+const EventsPage = lazy(() => import('./pages/EventsPage'));
+const CareerPage = lazy(() => import('./pages/CareerPage'));
+const ProjectPage = lazy(() => import('./pages/ProjectsPage'));
+const NewsDetailPage = lazy(() => import('./pages/NewsDetailPage'));
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+const ErrorPage = lazy(() => import('./components/common/Error'));
 
 function App() {
   return (
@@ -16,16 +19,18 @@ function App() {
       <div className="app">
         <Header />
         <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/events" element={<EventsPage />} />
-            <Route path="/career" element={<CareerPage />} />
-            <Route path="/projects" element={<ProjectPage />} />
-            <Route path="/news/:newsTitle" element={<NewsDetailPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/news" element={<NewsPage />} />
+              <Route path="/events" element={<EventsPage />} />
+              <Route path="/career" element={<CareerPage />} />
+              <Route path="/projects" element={<ProjectPage />} />
+              <Route path="/news/:newsTitle" element={<NewsDetailPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </Suspense>
         </main>
         <Footer />
       </div>
